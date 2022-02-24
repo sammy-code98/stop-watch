76a2c341f3e6220a3ff409dc393fc4453b252657
sammy-code98 <sammysmart98@yahoo.com>
2022-02-24 22:55:38 +0100

    feat: update js file

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("kekekek");
+
+// first conver time to hours, minutes , seconds and milisecond formart
+
+
+function timeToString(time){
+    let  hrDiff = time / 3600000;
+    let hrs = Math.floor(hrDiff)
+}
